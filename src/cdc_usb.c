652a70f19efce9bd0b8ca53b4eb4b4f87cb8eb3c
LittleAntares <kseangchy@gmail.com>
2024-12-29 14:51:37 +0000

    [NanoPB-Pico] Adding RGB Light function for RP2040 Zero
    * Adding feature RGB lighting function for RP2040 Zero Board via ws2812.pio, cdc_usb.c and cdc_usb.h
    * Updating CMakeLists.txt for it

---
+++ src/cdc_usb.c
@@ -0,0 +1,13 @@
+#include "cdc_usb.h"
+
+
+
+void put_pixel(uint32_t pixel_grb)
+{
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+void put_rgb(uint8_t red, uint8_t green, uint8_t blue)
+{
+    uint32_t mask = (green << 16) | (red << 8) | (blue << 0);
+    put_pixel(mask);
+}
--- src/main.c
+++ src/main.c
@@ -1,13 +1,20 @@
-#include <pb_encode.h>
-#include <pb_decode.h>
-#include "uart.pb.h"
 #include "stdio.h"
+
 #include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+
 #include "stdlib.h"
 #include "nanopb.h"
 
+#include "cdc_usb.h"
+
+
 #define BUFFER_SIZE 128
 
+//Board Macro
+#define RP2040_ZERO_BOARD 1
+#define RASP_PIPICO_BOARD 0
 
 // Main function
 int main(int argc, char **argv) {
@@ -17,8 +24,22 @@ int main(int argc, char **argv) {
     sleep_ms(1000);
     data_t buffer;
     uint8_t status;
-    
 
+
+    //Start LED
+    if(RP2040_ZERO_BOARD){
+        PIO pio = pio0;
+        int sm = 0;
+        uint offset = pio_add_program(pio, &ws2812_program);
+        
+        ws2812_program_init(pio, sm, offset, 16, 800000, true);
+
+        //Setting LED to Red during startup
+        put_rgb(252, 0 , 0);
+    }
+
+
+    put_rgb(0, 252, 0);
 
     return 0;
 }
