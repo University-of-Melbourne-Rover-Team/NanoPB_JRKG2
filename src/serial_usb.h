6736640ac864010519a63512823ad0bf16088a9b
LittleAntares <kseangchy@gmail.com>
2024-12-29 14:51:37 +0000

    [NanoPB-Pico] Adding RGB Light function for RP2040 Zero
    * Adding feature RGB lighting function for RP2040 Zero Board via ws2812.pio, cdc_usb.c and cdc_usb.h
    * Updating CMakeLists.txt for it

---
+++ src/serial_usb.h
@@ -0,0 +1,13 @@
+#ifndef SERIAL_USB_H
+#define SERIAL_USB_H
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "generated/ws2812.pio.h"
+
+void put_pixel(uint32_t pixel_grb);
+void put_rgb(uint8_t red, uint8_t green, uint8_t blue);
+
+
+#endif
